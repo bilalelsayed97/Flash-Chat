b4086d727c6c00255b4e3fe297a790ee0f8211f2
bilal elsayed <bilalelsayed97@gmail.com>
2024-02-19 17:49:44 +0200

    table view

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -6,8 +6,15 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var message: [Message] = [
+    Message(sender: "1@2.com", body: "Hey"),
+    Message(sender: "a@b.com", body: "Hello"),
+    Message(sender: "1@2.com", body: "What's up?"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
 
     }
@@ -25,4 +32,16 @@ class ChatViewController: UIViewController {
         }
     }
     
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.message.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel = message[indexPath].body
+        return cell
+    }
 }
--- Flash Chat iOS13/Models/Message.swift
+++ Flash Chat iOS13/Models/Message.swift
@@ -1,5 +1,4 @@
 import Foundation
-
 struct Message {
     let sender: String
     let body: String
